454a177fe825c50ab92523df90c1113ecf2513aa
Crowdbotics <team@crowdbotics.com>
2021-09-01 15:36:09 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "restless_butterfly_30251Identifier",
+  url: "https://restless_butterfly_30251Identifier.botics.co",
+  api: "https://restless_butterfly_30251Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
